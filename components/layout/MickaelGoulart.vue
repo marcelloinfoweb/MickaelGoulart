e2978e9aa9c6ec6b530a632b41e61f969dad2fc6
Marcelo Caeatno <marcelloinfoweb@gmail.com>
2023-08-10 15:21:40 -0300

    Ajustes: texto, imagem header e botão WA

--- components/layout/MickaelGoulart.vue
+++ components/layout/MickaelGoulart.vue
@@ -19,25 +19,27 @@ export default defineComponent({
       </div>
       <div data-aos="fade-up-right" class="flex items-center flex-wrap">
         <div class="w-5/6 sm:w-1/2 p-6">
-          <h3 class="text-3xl text-gray-800 font-bold leading-none mb-3">
+          <h4 class="text-3xl text-gray-800 font-bold leading-none mb-3">
             Mickael Goulart / CREFITO 117193F
-          </h3>
-          <p class="text-gray-600 mb-8">
-            Fisioterapeuta especialista em quiropraxia, instrumental e osteopatia.
-          </p>
-          <p class="text-gray-600 mb-8">
-            Mais de 100 mil consultas e atendimentos com mais de 15 anos de
-            experiência clínica.
-          </p>
-          <p class="text-gray-600 mb-8">
-            Não espere a dor chegar para se cuidar! Agende agora mesmo sua
-            avaliação com o melhor especialista da região!
-          </p>
+          </h4>
+          <div class="text-gray-700">
+            <p class="mb-8">
+              Fisioterapeuta especialista em quiropraxia, instrumental e osteopatia.
+            </p>
+            <p class="mb-8">
+              Mais de 100 mil consultas e atendimentos com mais de 15 anos de
+              experiência clínica.
+            </p>
+            <p class="mb-8">
+              Não espere a dor chegar para se cuidar! Agende agora mesmo sua
+              avaliação com o melhor especialista da região!
+            </p>
+          </div>
           <div data-aos="zoom-in-down" class="inset-0 flex items-center justify-center">
             <layout-botao-quero-agendar />
           </div>
         </div>
-        <div class="w-full sm:w-1/2 p-6">
+        <div class="sm:w-1/2 p-6">
           <nuxt-img format="webp" src="/imgs/Mickael_Goulart.png" sizes="sm:100vw md:50vw lg:300px" alt="Mickael Goulart"
             class="rounded-full border-8" />
         </div>
