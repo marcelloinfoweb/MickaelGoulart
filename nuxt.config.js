953ca62a17ca73717f7f1d78abb43a731790744d
Marcelo Caetano / <marcelloinfoweb@gmail.com>
2023-08-24 18:51:56 +0000

    Add Gtag .

--- nuxt.config.js
+++ nuxt.config.js
@@ -47,7 +47,7 @@ export default {
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
     { src: "~/plugins/aos.js", mode: "client" },
-    { src: "~/plugins/vue-gtag.js" },
+    
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
